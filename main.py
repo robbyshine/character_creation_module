9383b05596046326ec5c2045039912175d3f43c9
Сергей Лопатко <rasta116@mail.ru>
2023-04-11 15:30:01 +0300

    cade according to PEP8

--- main.py
+++ main.py
@@ -3,34 +3,36 @@ from random import randint
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
-        return f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}'
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный'
-                ' {5 + randint(-3, -1)}')
-    return f'{char_name} не нанёс урон'
+        return (f'{char_name} нанёс урон противнику равный '
+                f'{5 + randint(-3, -1)}')
+    return (f'{char_name} не нанёс урон')
 
 
 def defence(char_name, char_class):
     if char_class == 'warrior':
-        return f'{char_name} блокировал {10 + randint(5, 10)} урона'
+        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
-        return f'{char_name} блокировал {10 + randint(-2, 2)} урона'
+        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
-        return f'{char_name} блокировал {10 + randint(2, 5)} урона'
-    return f'{char_name} не блокировал урон'
+        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+    return (f'{char_name} не блокировал урон')
 
 
 def special(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
-                '«Выносливость {80 + 25}»')
+                f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return f'{char_name} применил специальное умение «Атака {5 + 40}»'
+        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
-        return f'{char_name} применил специальное умение «Защита {10 + 30}»'
-    return f'{char_name} не применил специальное умение'
+        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return (f'{char_name} не применил специальное умение')
 
 
 def start_training(char_name, char_class):
